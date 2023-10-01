061e86ae060e9c8f0e0c77f7a8ce3b2b10c7d990
Kate-Mil <114938475+Kate-Mil@users.noreply.github.com>
2023-10-01 04:28:23 +0300

    refactoring code of getAllContacts and getContactById, make addContact, install joi

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,6 +1,7 @@
 // const express = require('express')
 import express from "express";
 import * as contactsServer from "../../models/contacts.js";
+import { HttpError } from "../../helpers/index.js";
 
 const router = express.Router();
 
@@ -9,23 +10,38 @@ router.get("/", async (req, res, next) => {
     const result = await contactsServer.listContacts();
     res.json(result);
   } catch (error) {
-    res.status(500).json({ messege: "Server error" });
+    // res.status(500).json({ message: "Server error" });
+    next(error);
   }
 });
 
 router.get("/:contactId", async (req, res, next) => {
   try {
-    const contactId = req.params.contactId;
+    const { contactId } = req.params;
     const result = await contactsServer.getContactById(contactId);
+    if (!result) {
+      throw HttpError(404, `Contact with ${contactId} is not found`);
+      // const error = new Error(`Contact with ${contactId} is not found`);
+      // error.status = 404;
+      // throw error;
+      // return res.status(404)
+      //   .json({ message: `Contact with ${contactId} is not found` });
+    }
     res.json(result);
   } catch (error) {
-    res.status(500).json({ messege: "Server error" });
+    // const { status = 500, message = "Server error" } = error;
+    // res.status(status).json({ message });
+    next(error);
   }
 });
 
 router.post("/", async (req, res, next) => {
-  const result = await contactsServer.addContact(body);
-  res.json(result);
+  try {
+    const result = await contactsServer.addContact(req.body);
+    res.status(201).json(result);
+  } catch (error) {
+    next(error);
+  }
 });
 
 router.delete("/:contactId", async (req, res, next) => {
