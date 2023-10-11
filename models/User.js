b70d6c10d0e3f77fe72d7a917a9787a7a3322a16
Kate-Mil <114938475+Kate-Mil@users.noreply.github.com>
2023-10-11 23:41:51 +0300

    make mongoose and Joi schemas

---
+++ models/User.js
@@ -0,0 +1,40 @@
+import { Schema, model } from "mongoose";
+import Joi from "joi";
+import { handleSaveError, runValidatorsAtUpdate } from "./hooks.js";
+
+const emailRegexp = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+const userSchema = new Schema(
+  {
+    password: {
+      type: String,
+      minlength: 6,
+      required: [true, "Set password for user"],
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      match: emailRegexp,
+      unique: true,
+    },
+    subscription: {
+      type: String,
+      enum: ["starter", "pro", "business"],
+      default: "starter",
+    },
+    token: String,
+  },
+  { versionKey: false, timestamps: true }
+);
+
+userSchema.post("save", handleSaveError);
+userSchema.pre("findOneAndUpdate", runValidatorsAtUpdate);
+userSchema.post("findOneAndUpdate", handleSaveError);
+
+const userAddSchema = Joi.object({
+  email: Joi.string().pattern(emailRegexp).required(),
+  password: Joi.string().min(6).required(),
+});
+
+const User = model("user", userSchema);
+export default User;
